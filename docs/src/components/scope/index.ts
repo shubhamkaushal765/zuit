7c158b27192212edbd20c892a7e6d558992a2df6
Shubham Kaushal <shubhamkaushal765@gmail.com>
2026-05-15 15:58:14 +0800

    chore(docs): scope barrel export

---
+++ docs/src/components/scope/index.ts
@@ -0,0 +1,12 @@
+export { default as CategoryBadge } from './CategoryBadge';
+export type { BadgeTone, CategoryBadgeProps } from './CategoryBadge';
+export { default as StatRow } from './StatRow';
+export type { Stat, StatAccent, StatRowProps } from './StatRow';
+export { default as LangDots } from './LangDots';
+export type { LangFlags, LangDotsProps } from './LangDots';
+export { default as FilterableTable } from './FilterableTable';
+export type {
+  FilterableTableColumn,
+  FilterableTableProps,
+  FilterOption,
+} from './FilterableTable';
